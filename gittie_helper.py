9f8cb229b7397498df9cbb4e29126f7c4f1c665d
Rafał Stępień <rafalstepien@Rafas-MacBook-Pro.local>
2017-01-17 13:06:41 +0100

    Initial commit - add gitignore file and template of GittieHelper class

---
+++ gittie_helper.py
@@ -0,0 +1,42 @@
+class GittieHelper():
+
+    def __init__(self):
+        """
+        Initialize attributes with default value
+        """
+        pass
+
+    def set_temperature(self, temperature_degree):
+        """
+        Method sets temperature to attribute and validate input
+        :param temperature_degree:
+        """
+        pass
+
+    def set_humidity(self, humidity_value):
+        """
+        Method sets humidity level to attribute and validate input
+        :param humidity_value:
+        """
+        pass
+
+    def set_air_pollution(self, air_pollution_level):
+        """
+        Method sets air pollution level to attribute and validate input
+        :param air_pollution_level:
+        """
+        pass
+
+    def set_day_of_the_year(self, day_number):
+        """
+        Method sets day number from beginning of the year to attribute and validate input
+        :param day_number:
+        """
+        pass
+
+    def get_value(self):
+        """
+        Method should calculate if exiting home is safe for gittie
+        :param day_number:
+        """
+        pass
